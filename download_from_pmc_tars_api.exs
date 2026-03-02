0186c6705c32bb2aeee5f58b201b8f3a8c6ffa00
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-01 22:31:45 -0800

    Added Elixir Download Script And Removed Worktrees

---
+++ download_from_pmc_tars_api.exs
@@ -0,0 +1,46 @@
+Mix.install([
+  {:req, "~> 0.4"},
+  {:yaml_elixir, "~> 2.9"}
+])
+
+[dir] = System.argv()
+
+table_configs =
+  dir <> "*.yaml"
+  |> Path.wildcard()
+
+Enum.each(table_configs, fn yaml_file ->
+  try do
+    data = YamlElixir.read_from_file!(yaml_file)
+
+    file_name =
+      data["template"]["source"]["url"]
+      |> Path.basename()
+
+    pmc_id = data["template"]["provenance"]["publication"]
+    local_path = data["template"]["source"]["local"]
+
+    url = "http://localhost:8051/extract-from-tar?" <>
+      URI.encode_query(%{
+        "filename" => "#{pmc_id}/#{file_name}",
+        "tarpath" => "/15TB_1/users/gglusman/PMC/tars/#{String.slice(pmc_id, -3..-1)}/#{pmc_id}.tar.xz"
+      })
+
+    unless File.exists?(local_path) do
+      case Req.get(url) do
+        {:ok, %{status: 200, body: body}} ->
+          File.write!(local_path, body)
+
+        {:ok, %{status: status}} ->
+          IO.puts("CODE 03 | HTTP ERROR #{status} FOR #{yaml_file}")
+
+        {:error, reason} ->
+          IO.puts("CODE 04 | REQUEST FAILED FOR #{yaml_file}: #{inspect(reason)}")
+      end
+    end
+
+    IO.puts("CODE 01 | SUCCEDED #{yaml_file} | OK")
+  rescue
+    e -> IO.puts("CODE 02 | FAILED #{yaml_file}: #{Exception.message(e)}")
+  end
+end)
